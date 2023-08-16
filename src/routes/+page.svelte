e2513b856f64ed5ea33b9748d06eef34e84a90e9
Saumya Swarup <swarupsaumya65@gmail.com>
2023-08-16 23:19:45 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Simpl Wishlist" />
+</svelte:head>
+
+<section>
+	hi
+
+</section>
+
+<style>
+	
+</style>
